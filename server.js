68797c196e9e0dd03961c5b6fac019e59a0c0dad
AMAN GUPTA <66153885+amangupta9229@users.noreply.github.com>
2020-10-26 18:49:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,47 @@
 const schedule = require("node-schedule")
-var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://temp:temp123@52.172.181.199:32338/DB-singlecoll-test';
+let MongoClient = require('mongodb').MongoClient;
+let url = 'mongodb://temp:temp123@52.172.181.199:32338/DB-singlecoll-test';
 
 
 function getData(y){
     return y.find({status:1}).limit(50);
 }
 
-var TotalDuration=[]
+let TotalDuration=[]
 
 function runLoop(response){
     
-    var j=0;
+    let j=0;
     response.each(function(err, doc) {
         
-            var totalDuration=0;
+            let totalDuration=0;
            
-            for(var i=0; i < doc.attempts.length; i++){
+            if(doc!=null){
+            for(let i=0; i < doc.attempts.length; i++){
 
-                var value = doc.attempts[i];
+                
+                let value = doc.attempts[i];
                 totalDuration+=value.duration;
+                
             }
-            
-            var temp=doc;
+        
+        
+            let temp=doc;
             temp.TotalDuration=totalDuration;
             console.log(temp)
             TotalDuration[j]=totalDuration;
             j++;
+        }
          });
 
 }
 
-schedule.scheduleJob( '*/5 * * * * *', () =>{
+schedule.scheduleJob( '0 0 * * *', () =>{
     MongoClient.connect(url, function (err, client) {
         if (err) throw err;
 
-        var db = client.db('DB-singlecoll-test')
-        var cursor = db.collection('OnlineClassAttendance')
+        let db = client.db('DB-singlecoll-test')
+        let cursor = db.collection('OnlineClassAttendance')
         async function doWork(){
             const response = await getData(cursor)
             await runLoop(response,cursor)
